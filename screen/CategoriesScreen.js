3e0754dabbab7c98a43c763b94f915f008387d9a
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-18 14:51:06 +0800

    add categories grid and screen

---
+++ screen/CategoriesScreen.js
@@ -0,0 +1,26 @@
+import { FlatList } from 'react-native';
+
+import CategoryGridTile from '../components/CategoryGridTile';
+import { CATEGORIES } from '../data/dummy-data';
+
+function renderCategoryItem(itemData) {
+    return (
+        <CategoryGridTile
+            title={itemData.item.title}
+            color={itemData.item.color}
+        />
+    );
+}
+
+function CategoriesScreen() {
+    return (
+        <FlatList
+            data={CATEGORIES}
+            renderItem={renderCategoryItem}
+            keyExtractor={(item) => { item.id }}
+        >
+        </FlatList>
+    );
+}
+
+export default CategoriesScreen;
