f995b150bc63d775227baa1a34638fee542738b6
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-18 15:40:58 +0800

    add navigator in main screen

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import CategoriesScreen from './screen/CategoriesScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />
-      <CategoriesScreen />
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+        {/* <CategoriesScreen /> */}
+      </NavigationContainer>
     </>
   );
 }
