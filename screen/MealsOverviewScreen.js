24e26ef8931bea0c76df9eb68666503805bac73a
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-19 14:37:00 +0800

    adding meal item into mealoverview screen when press

--- screen/MealsOverviewScreen.js
+++ screen/MealsOverviewScreen.js
@@ -1,10 +1,30 @@
-import { Text, View, StyleSheet } from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
+//can use this as an alternative for get route params
+// import { useRoute } from "@react-navigation/native";
+import MealItem from "../components/MealItem";
+
 import { MEALS } from "../data/dummy-data";
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+  const cateId = route.params.categoryId;
+
+  const displayMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(cateId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return (
+      <MealItem title={itemData.item.title}></MealItem>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList
+        data={displayMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      ></FlatList>
     </View>
   );
 }
