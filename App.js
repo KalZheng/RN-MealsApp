34c631a83bbfee18f1bf5da289812001a7b7dc68
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-19 15:22:07 +0800

    add styling for navigation, for default to all and individual

--- App.js
+++ App.js
@@ -13,8 +13,20 @@ export default function App() {
     <>
       <StatusBar style='dark' />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        <Stack.Navigator
+          //this allow to set default styling for all screen
+          screenOptions={{
+            headerStyle: { backgroundColor: '#351401' },
+            headerTintColor: 'white',
+            contentStyle: { backgroundColor: '#3f2f25' }
+          }}>
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: 'All Categorires',
+            }}
+          />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
